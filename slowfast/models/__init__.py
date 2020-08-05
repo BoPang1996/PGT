ed41035476817782798c168c1eae6c0dadb60ffb
ecr23xx <370095872@qq.com>
2020-08-05 11:27:33 +0800

    minor update for kinetics.

--- slowfast/models/__init__.py
+++ slowfast/models/__init__.py
@@ -2,6 +2,6 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 
 from .build import MODEL_REGISTRY, build_model  # noqa
-from .backbones.i3d import ResNet # noqa
+from .backbones.resnet import ResNet # noqa
 from .backbones.r3d import RegNet # noqa
 from .backbones.slowfast import SlowFast  # noqa
