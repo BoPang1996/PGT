bbe7f97b4c8ed204156f8c2c8ecf45270fd99be8
ecr23xx <370095872@qq.com>
2020-11-09 09:42:39 +0800

    Fix bugs in mox launcher.

--- tools/mox_launcher.py
+++ tools/mox_launcher.py
@@ -1,99 +1,86 @@
-#!/usr/bin/env python3
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-
-"""Argument parser functions."""
-
-import argparse
+import os
+import re
 import sys
+import stat
+import subprocess
+import moxing as mox
+from argparse import ArgumentParser
+from urllib.parse import urlparse
+mox.file.shift("os", "mox")
 
-import slowfast.utils.checkpoint as cu
-from slowfast.config.defaults import get_cfg
+
+PIP_OBS_PATH = {
+    "bucket-5006": "obs://bucket-5006/penggao/pip_packages/progress-action",
+    "bucket-3010": "obs://bucket-3010/penggao/pip_packages/progress-action",
+    "video-understanding-shanghai": "obs://video-understanding-shanghai/penggao/pip_packages",
+}
+PIP_LOCAL_PATH = "/cache/pip_packages"
 
 
 def parse_args():
-    """
-    Parse the following arguments for a default parser for PySlowFast users.
-    Args:
-        shard_id (int): shard id for the current machine. Starts from 0 to
-            num_shards - 1. If single machine is used, then set shard id to 0.
-        num_shards (int): number of shards using by the job.
-        init_method (str): initialization method to launch the job with multiple
-            devices. Options includes TCP or shared file-system for
-            initialization. details can be find in
-            https://pytorch.org/docs/stable/distributed.html#tcp-initialization
-        cfg (str): path to the config file.
-        opts (argument): provide addtional options from the command line, it
-            overwrites the config loaded from file.
-        """
-    parser = argparse.ArgumentParser(
-        description="Provide SlowFast video training and testing pipeline."
-    )
-    parser.add_argument(
-        "--shard_id",
-        help="The shard id of current node, Starts from 0 to num_shards - 1",
-        default=0,
-        type=int,
-    )
-    parser.add_argument(
-        "--num_shards",
-        help="Number of shards using by the job",
-        default=1,
-        type=int,
-    )
-    parser.add_argument(
-        "--init_method",
-        help="Initialization method, includes TCP or shared file-system.",
-        default="auto",
-        type=str,
-    )
-    parser.add_argument(
-        "--cfg",
-        dest="cfg_file",
-        help="Path to the config file",
-        default="configs/Kinetics/SLOWFAST_4x16_R50.yaml",
-        type=str,
-    )
-    # moxing setting
-    parser.add_argument("--rank", help="Used by model arts", default=0, type=int)
-    parser.add_argument("--world_size", help="Used by model arts", default=1, type=int)
-    # no use args
+    parser = ArgumentParser(description="Moxing launcher")
+
     parser.add_argument("--data_url", help="No use", type=str)
     parser.add_argument("--train_url", help="No use", type=str)
     parser.add_argument("--lr", help="No use", type=float)
-    if len(sys.argv) == 1:
-        parser.print_help()
+    parser.add_argument("--cfg", help="Config file", type=str)
+    parser.add_argument("-install_pkg", default=False, action="store_true")
+    parser.add_argument("-install_log", default=False, action="store_true")
+
     return parser.parse_known_args()
 
 
-def load_config(args, opts=None):
-    """
-    Given the arguemnts, load and initialize the configs.
-    Args:
-        args (argument): arguments includes `shard_id`, `num_shards`,
-            `init_method`, `cfg_file`, and `opts`.
-    """
-    # Setup cfg.
-    cfg = get_cfg()
-    # Load config from cfg.
-    if args.cfg_file is not None:
-        cfg.merge_from_file(args.cfg_file)
-    # Load config from command line, overwrite config from opts.
-    if opts is not None:
-        cfg.merge_from_list(opts)
+def compile_custom():
+    FNULL = open(os.devnull, 'w')
 
-    # Inherit parameters from args.
-    if hasattr(args, "num_shards") and hasattr(args, "shard_id"):
-        cfg.NUM_SHARDS = args.num_shards
-        cfg.SHARD_ID = args.shard_id
-    if hasattr(args, "rng_seed"):
-        cfg.RNG_SEED = args.rng_seed
-    if hasattr(args, "output_dir"):
-        cfg.LOGS.DIR = args.output_dir
-    if hasattr(args, "world_size") and hasattr(args, "rank"):
-        cfg.NUM_SHARDS = args.world_size
-        cfg.SHARD_ID = args.rank
+    class sys_cd:
+        """Context manager for changing the current working directory"""
 
-    # Create the checkpoint dir.
-    cu.make_checkpoint_dir(cfg.LOGS.DIR)
-    cfg.freeze()
-    return cfg
+        def __init__(self, newPath):
+            self.newPath = os.path.expanduser(newPath)
+
+        def __enter__(self):
+            self.savedPath = os.getcwd()
+            os.chdir(self.newPath)
+
+        def __exit__(self, etype, value, traceback):
+            os.chdir(self.savedPath)
+    with sys_cd("progress-action"):
+        subprocess.call([sys.executable, "setup.py", "build", "develop"])
+
+
+def pip_install(package, log):
+    FNULL = open(os.devnull, 'w')
+    if log:
+        subprocess.call([sys.executable, "-m", "pip", "install", package])
+    else:
+        subprocess.call([sys.executable, "-m", "pip", "install", package],
+                        stdout=FNULL, stderr=subprocess.STDOUT)
+
+
+def pip_install_directory(bucket_name, install_log):
+    mox.file.copy_parallel(PIP_OBS_PATH[bucket_name], PIP_LOCAL_PATH)
+    print("Copy pip packages from obs finished!")
+
+    with open(os.path.join(PIP_LOCAL_PATH, "install_list.txt"), 'r') as list_file:
+        for package in list_file:
+            pip_install(os.path.join(PIP_LOCAL_PATH, package.strip()), install_log)
+    print("Install pip packages finished!")
+
+
+if __name__ == "__main__":
+    args, _ = parse_args()
+    bucket_name = urlparse(args.data_url).netloc
+
+    if args.install_pkg:
+        # upgrade pypi
+        subprocess.call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
+        # remove pyyaml or fvcore install will fail
+        subprocess.call([sys.executable, "-m", "pip", "install", "--ignore-installed", "PyYAML"])
+        pip_install_directory(bucket_name, args.install_log)
+    if "configs/AVA/" in args.cfg:
+        compile_custom()
+
+    sys.path.append("progress-action/")
+    from run_net import main
+    main()
